bd43682b78230b71300f2a95aa687075487bc47d
Kristjan Lääne <xonproject@gmail.com>
2024-09-17 19:47:30 +0300

    useLocalStorage gerenic types

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -122,7 +122,10 @@ export function useDebounce<T>(value: T, delay = 500): T {
 
 // ---------------------------------------------------------------------
 
-export function useLocalStorage(key: string, initialValue) {
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
   const [value, setValue] = useState(() =>
     JSON.parse(localStorage.getItem(key) || JSON.stringify(initialValue))
   );
