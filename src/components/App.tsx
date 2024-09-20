8e48ac09186a58c1fe36c900179aee11764338c0
Kristjan Lääne <xonproject@gmail.com>
2024-09-20 15:26:54 +0300

    jobItemsContextProvider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,48 +12,9 @@ import JobList from './JobList';
 import PaginationControls from './PaginationControls';
 import ResultsCount from './ResultsCount';
 import SortingControls from './SortingControls';
-import { useSearchQuery } from '../lib/hooks';
 import { Toaster } from 'react-hot-toast';
-import { RESULTS_PER_PAGE } from '../lib/constants';
-import { PageDirection, SortBy } from '../lib/types';
 
 function App() {
-  // state
-  const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [sortBy, setSortBy] = useState<SortBy>('relevant');
-
-  // derived / computed state
-  const totalNumberOfResults = jobItems?.length || 0;
-  const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
-  const jobItemsSorted =
-    [...(jobItems || [])].sort((a, b) => {
-      if (sortBy === 'relevant') {
-        return b.relevanceScore - a.relevanceScore;
-      } else {
-        return a.daysAgo - b.daysAgo;
-      }
-    }) || [];
-
-  const jobItemsSortedAndSliced = jobItemsSorted?.slice(
-    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-    currentPage * RESULTS_PER_PAGE
-  );
-
-  // event handlers / actions
-
-  const handleChangePage = (direction: PageDirection) => {
-    if (direction === 'next') {
-      setCurrentPage((prev) => prev + 1);
-    } else if (direction === 'previous') {
-      setCurrentPage((prev) => prev - 1);
-    }
-  };
-  const handleChangeSortBy = (newSortBy: SortBy) => {
-    setSortBy(newSortBy);
-    setCurrentPage(1);
-  };
-
   return (
     <>
       <Background />
